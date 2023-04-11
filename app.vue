aa98a663e6ba529ba8852e8326dedd046904fb58
Zack Hatlen <zckhtln@icloud.com>
2023-04-11 11:30:56 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -2,8 +2,13 @@
   <div class="flex">
     <div class="p-1 box-content w-[calc(100%+40px)] mx-auto max-w-[935px] grow">
       <div class="pb-1 text-left">
-        <select v-model="selectedColor">
+        <select>
           <option v-for="(color, i) in colors" :key="i" :value="color.name">{{ color.name }}</option>
+        </select>
+      </div>
+      <div class="pb-1 text-left">
+        <select>
+          <option v-for="(size, i) in sizes" :key="i" :value="size.name">{{ size.name }}</option>
         </select>
       </div>
       <div class="pb-1 text-right">
@@ -35,26 +40,23 @@ const colorMode = useColorMode()
 const allProducts = ref([])
 const endCursor = ref(null)
 const loading = ref(false)
-const selectedColor = ref('')
+const colors = ref([])
+const sizes = ref([])
 
 const { allPaRenk } = await GqlGetAllPaRenk()
-
-const colors = ref([])
-
-if (allPaRenk?.nodes) {
-  colors.value = allPaRenk.nodes.filter(color => color.products.nodes.length)
-}
+colors.value = allPaRenk.nodes.filter(color => color.products.nodes.length)
 
 while (allPaRenk?.pageInfo?.hasNextPage) {
   const response = await GqlGetAllPaRenk({
     after: allPaRenk.pageInfo.endCursor,
   })
-  if (response?.allPaRenk?.nodes) {
-    const filteredColors = response.allPaRenk.nodes.filter(color => color.products.nodes.length)
-    colors.value = colors.value.concat(filteredColors)
-  }
+  const filteredColors = response.allPaRenk.nodes.filter(color => color.products.nodes.length)
+  colors.value = colors.value.concat(filteredColors)
   allPaRenk.pageInfo = response.allPaRenk.pageInfo
 }
+
+const { allPaBeden } = await GqlGetAllPaBeden()
+sizes.value = allPaBeden.nodes.filter(size => size.products.nodes.length)
 
 async function fetchProducts() {
   loading.value = true
