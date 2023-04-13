eb3089442f5cadf5d4d9d67c0a07dbdba768d17e
Zack Hatlen <zckhtln@icloud.com>
2023-04-13 17:35:20 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -16,7 +16,7 @@
         </select>
       </div>
       <div class="grid gap-1 grid-cols-3 grid-rows-3">
-        <div v-if="loading" v-for="node in 9" :key="node" class="bg-neutral-200 dark:bg-neutral-800">
+        <div v-if="loading" v-for="node in 9" :key="node" class="animate-pulse bg-neutral-200 dark:bg-neutral-800">
           <div class="relative pb-[133%] overflow-hidden"></div>
         </div>
         <div v-else v-for="node in allProducts" class="bg-neutral-200 dark:bg-neutral-800">
