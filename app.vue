d3e2c97e5ecc606c172ff8b7c9ee578efa2b8d6a
Zack Hatlen <zckhtln@icloud.com>
2023-04-24 16:26:06 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -31,20 +31,33 @@
         </select>
       </div>
       <div role="tablist" class="border-t dark:border-neutral-800 box-border justify-end flex gap-[60px] relative">
-        <div class="items-center dark:text-[#a8a8a8] text-xs cursor-pointer flex h-[52px] justify-center">
+        <div @click.stop="isDropdownCategory = !isDropdownCategory; isDropdownSortBy = false;" :class="{activeTab: isDropdownCategory}" class="items-center dark:text-[#a8a8a8] text-xs flex h-[52px] justify-center cursor-pointer">
           <div class="flex box-border items-center">
             <Icon name="system-uicons:grid-squares" size="21" />
-            <span class="ml-1.5 font-semibold uppercase tracking-wider">Category</span>
-          </div>
-        </div>
-        <div @click.stop="isDropdown = !isDropdown" class="items-center dark:text-[#a8a8a8] text-xs cursor-pointer flex h-[52px] justify-center">
-          <div class="flex box-border items-center">
-            <Icon name="system-uicons:filter" size="21" />
-            <span class="ml-1.5 font-semibold uppercase tracking-wider">Sort by</span>
+            <span class="ml-1.5 font-semibold uppercase tracking-wider select-none">Category</span>
           </div>
           <Transition>
-            <div v-show="isDropdown" class="absolute top-full z-10 -right-[18px] dropdown">
-              <div class="dropdown-triangle"></div>
+            <div v-show="isDropdownCategory" class="absolute top-full z-10 right-[55px] dropdown">
+              <div class="dropdown-triangle left-[30%]"></div>
+              <div class="text-sm bg-white dark:bg-neutral-800 rounded-2xl overflow-hidden">
+                <div class="border-b dark:border-[#353535] last:border-b-0" @click="selectedCategory = ''">
+                  <a class="dark:text-neutral-100 block px-4 py-2.5 hover:dark:bg-[#3c3c3c] hover:transition-all">All Categories</a>
+                </div>
+                <div class="border-b dark:border-[#353535] last:border-b-0" v-for="category in categories" :key="category.id" @click="selectedCategory = category.name">
+                  <a class="dark:text-neutral-100 block px-4 py-2.5 hover:dark:bg-[#3c3c3c] hover:transition-all">{{ category.name }}</a>
+                </div>
+              </div>
+            </div>
+          </Transition>
+        </div>
+        <div @click.stop="isDropdownSortBy = !isDropdownSortBy; isDropdownCategory = false;" :class="{activeTab: isDropdownSortBy}" class="items-center dark:text-[#a8a8a8] text-xs flex h-[52px] justify-center cursor-pointer">
+          <div class="flex box-border items-center">
+            <Icon name="system-uicons:filter" size="21" />
+            <span class="ml-1.5 font-semibold uppercase tracking-wider select-none">Sort by</span>
+          </div>
+          <Transition>
+            <div v-show="isDropdownSortBy" class="absolute top-full z-10 -right-[18px] dropdown">
+              <div class="dropdown-triangle left-[57%]"></div>
               <div class="text-sm bg-white dark:bg-neutral-800 rounded-2xl overflow-hidden">
                 <div class="border-b dark:border-[#353535] last:border-b-0" v-for="(option, index) in options" :key="index" @click="selectedOption = option.value">
                   <a class="dark:text-neutral-100 block px-4 py-2.5 hover:dark:bg-[#3c3c3c] hover:transition-all">{{ option.label }}</a>
@@ -77,7 +90,8 @@
 <script setup>
 import getProducts from "~/gql/queries/getProducts.gql"
 import getCategories from "~/gql/queries/getCategories.gql"
-const isDropdown = ref(false)
+const isDropdownSortBy = ref(false)
+const isDropdownCategory = ref(false)
 const router = useRouter()
 const route = useRoute()
 const searchTerm = ref(route.query.search || null)
@@ -142,7 +156,8 @@ const handleScroll = () => {
 
 const handleClickOutside = (event) => {
   if (!event.target.closest('.dropdown')) {
-    isDropdown.value = false;
+    isDropdownSortBy.value = false;
+    isDropdownCategory.value = false;
   }
 };
 
@@ -157,13 +172,6 @@ onUnmounted(() => {
 })
 
 watch([selectedOption, searchTerm, selectedCategory], ([newSelectedOption, newSearchTerm, newCategory]) => {
-  if (newSearchTerm !== null && newCategory !== '') {
-    if (newSearchTerm) {
-      newCategory = '';
-    } else if (!newCategory) {
-      newSearchTerm = null;
-    }
-  }
   switch (newSelectedOption) {
     case 'newest':
       sortByOrder.value = 'DESC'
@@ -201,7 +209,6 @@ watch([selectedOption, searchTerm, selectedCategory], ([newSelectedOption, newSe
 .dropdown-triangle {
   @apply absolute dark:bg-neutral-800 h-5 w-4 -top-[10px] rotate-90;
   clip-path: path('M8 0C8 4 9.32406e-08 7.819 1.25211e-07 10.5C1.57188e-07 13.1815 8 17.0005 8 21L8 0Z');
-  left: calc(50% - -16px);
 }
 .v-enter-active {
   @apply transition ease-out duration-100
@@ -218,6 +225,9 @@ watch([selectedOption, searchTerm, selectedCategory], ([newSelectedOption, newSe
 
 .v-leave-active {
   @apply transition ease-in duration-75
-} 
-
+}
+.activeTab {
+  @apply text-neutral-100;
+  box-shadow: 0 -1px 0 #fff;
+}
 </style>
