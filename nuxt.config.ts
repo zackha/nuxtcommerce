3d415fd20fe9c5fcc9f8dd40b52e0dd20984eb4a
Zack Hatlen <zckhtln@icloud.com>
2023-05-03 09:59:38 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 export default defineNuxtConfig({
+  ssr: false,
   modules: ['@nuxt/image-edge', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxtjs/apollo', 'nuxt-icon'],
   apollo: {
     clients: {
