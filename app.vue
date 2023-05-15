f6ef17fd18a298f9a2dae62cb6ddb95196f434c1
Zack Hatlen <zckhtln@icloud.com>
2023-05-15 18:10:05 +0300

    fix: add product card and empty component

--- app.vue
+++ app.vue
@@ -97,59 +97,16 @@
         </div>
       </div>
       <div class="grid grid-cols-3 gap-4">
-        <div
-          v-for="node in products"
-          :key="node.id"
-          class="group cursor-pointer rounded-2xl dark:bg-secondary-bg-d hover:dark:bg-[#222222] px-4 pt-4 pb-3 transition ease-[ease] duration-300 dark:border-[#222222] hover:dark:border-[#303030] border">
-          <div class="relative mb-4 pb-[133%] dark:shadow-[0_8px_24px_rgba(0,0,0,.5)] rounded-lg overflow-hidden">
-            <NuxtImg
-              loading="lazy"
-              :title="node.name"
-              :alt="node.image.altText || node.name"
-              :src="node.galleryImages.nodes[0].sourceUrl"
-              class="absolute h-full w-full dark:bg-neutral-800 bg-neutral-200 object-cover" />
-            <NuxtImg
-              loading="lazy"
-              :title="node.name"
-              :alt="node.image.altText || node.name"
-              :src="node.image.sourceUrl"
-              class="absolute h-full w-full dark:bg-neutral-800 bg-neutral-200 object-cover transition-opacity duration-300 group-hover:opacity-0" />
-          </div>
-          <div class="mb-1 grid grid-flow-col gap-6 px-2 text-sm font-semibold">
-            <div class="flex min-w-0 flex-col justify-between gap-1">
-              <div class="text-xs text-secondary-text dark:text-secondary-text-d transition-all hover:text-black hover:dark:text-neutral-100">
-                {{ node.allPaUrunTipi.nodes[0].name }}
-              </div>
-              <div :title="node.name" class="overflow-hidden text-ellipsis whitespace-nowrap">
-                {{ node.name }}
-              </div>
-            </div>
-            <div class="flex flex-col gap-1 text-right">
-              <div class="text-xs text-secondary-text dark:text-secondary-text-d line-through" v-html="node.regularPrice"></div>
-              <div class="font-bold text-[#d9000c] dark:text-[#ff0000]" v-html="node.salePrice"></div>
-            </div>
-          </div>
-        </div>
+        <ProductCard v-for="product in products" :key="product.id" :product="product" />
         <ProductSkeleton v-if="loading" />
       </div>
-      <div v-if="!empty && !loading" class="p-6 text-center text-lg">
-        <Icon name="majesticons:search-line" size="99"></Icon>
-        <div class="py-4">
-          <span v-if="selectedCategory">
-            In the <strong>{{ selectedCategory }}</strong> category,
-          </span>
-          <span v-if="searchTerm">
-            there are no items matching for: <strong>{{ searchTerm }}</strong>
-          </span>
-        </div>
-        <div class="text-sm">Try improving your results by double checking your spelling or trying a more general keyword.</div>
-      </div>
+      <ProductEmpty v-if="!empty && !loading" :cat="selectedCategory" :term="searchTerm" />
       <Footer />
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 const { products, empty, loading, categories, options, searchTerm, selectedCategory, selectedOption, isDropdownCategory, isDropdownSortBy } = useSearch();
 </script>
 
---
+++ components/ProductEmpty.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="p-6 text-center text-lg">
+    <Icon name="majesticons:search-line" size="99"></Icon>
+    <div class="py-4">
+      <span v-if="cat">
+        In the <strong>{{ cat }}</strong> category,
+      </span>
+      <span v-if="term">
+        there are no items matching for: <strong>{{ term }}</strong>
+      </span>
+    </div>
+    <div class="text-sm">Try improving your results by double checking your spelling or trying a more general keyword.</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  cat: { type: String, default: null },
+  term: { type: String, default: null },
+});
+</script>
