8cc5082461ec906eef6dec8a70b37f7ff106154b
Zack Hatlen <zckhtln@icloud.com>
2023-06-22 18:08:48 +0300

    Update AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -11,7 +11,7 @@
       <div class="relative flex flex-shrink flex-grow flex-col text-sm font-semibold text-neutral-600 px-2">
         <form class="group flex h-12 flex-grow rounded-full bg-[#e9e9e9] pl-4 pr-3 transition-all hover:bg-[#e1e1e1]">
           <div class="flex w-full items-center gap-4">
-            <div class="flex text-neutral-500 dark:text-neutral-400">
+            <div v-if="!suggestionMenu" class="flex text-neutral-500 dark:text-neutral-400">
               <Icon name="iconamoon:search-bold" size="20" />
             </div>
             <div class="flex w-full">
@@ -37,7 +37,7 @@
               <div class="p-6 text-center text-lg">
                 <Icon name="majesticons:search-line" size="99"></Icon>
                 <div class="py-4">
-                  <span v-if="searchQuery">
+                  <span>
                     there are no items matching for: <strong>{{ searchQuery }}</strong>
                   </span>
                 </div>
@@ -48,12 +48,16 @@
             <div v-else>
               <div v-if="!searchQuery" class="font-semibold text-base text-[#111] my-2">New</div>
               <div class="items-center gap-2 flex">
-                <NuxtLink :to="`/product/${product.slug}-${product.sku.split('-')[0]}`" v-for="product in searchResult" class="flex items-center border border-red-700 rounded-xl">
-                  <div class="h-28 rounded-lg overflow-hidden m-1 border border-red-700">
-                    <NuxtImg class="w-full h-full" :src="product.image.sourceUrl" />
+                <NuxtLink :to="`/product/${product.slug}-${product.sku.split('-')[0]}`" v-for="product in searchResult" class="flex bg-black/5 rounded-2xl overflow-hidden">
+                  <div class="relative w-32 h-32">
+                    <div class="h-full relative box-border">
+                      <NuxtImg class="object-cover absolute w-full h-full" :src="product.image.sourceUrl" />
+                    </div>
                   </div>
-                  <div class="">
-                    {{ product.name }}
+                  <div class="p-4 items-center flex">
+                    <div class="text-base text-[#111]">
+                      {{ product.name }}
+                    </div>
                   </div>
                 </NuxtLink>
               </div>
