c33c0e640b017646411dc1ba80f6abb1a9723b23
Zack Hatlen <zckhtln@icloud.com>
2023-06-23 14:57:02 +0300

    Update ProductCard.vue

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,7 +1,7 @@
 <template>
   <article v-for="product in products">
     <NuxtLink :to="`/product/${product.slug}-${product.sku.split('-')[0]}`" class="group">
-      <div class="cursor-pointer pt-4 pb-3 transition ease-[ease] duration-300">
+      <div class="cursor-pointer transition ease-[ease] duration-300">
         <div class="relative pb-[133%] dark:shadow-[0_8px_24px_rgba(0,0,0,.5)] rounded-2xl overflow-hidden">
           <NuxtImg
             loading="lazy"
