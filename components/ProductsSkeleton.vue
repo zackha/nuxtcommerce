cb59185916723c7c78cfb9032bcf687667c9bc31
Zack Hatlen <zckhtln@icloud.com>
2023-07-03 05:09:32 +0300

    feat: redesign products page skeleton

--- components/ProductsSkeleton.vue
+++ components/ProductsSkeleton.vue
@@ -1,17 +1,33 @@
 <template>
-  <div v-for="i in 21" :key="i" class="animate-pulse rounded-2xl border dark:bg-secondary-bg-d px-4 pt-4 pb-3 dark:border-[#222222]">
-    <div class="relative mb-4 pb-[133%] dark:shadow-[0_8px_24px_rgba(0,0,0,.5)] rounded-lg overflow-hidden">
-      <div class="absolute h-full w-full bg-neutral-200 dark:bg-neutral-800"></div>
-    </div>
-    <div class="mb-1 grid grid-flow-row-dense grid-cols-3 px-2">
-      <div class="col-span-2 flex min-w-0 flex-col justify-between gap-2">
-        <div class="h-8 w-2/4 rounded bg-neutral-200 dark:bg-neutral-800"></div>
-        <div class="h-full w-full rounded bg-neutral-200 dark:bg-neutral-800"></div>
-      </div>
-      <div class="flex flex-col items-end gap-2">
-        <div class="h-8 w-2/3 rounded bg-neutral-200 dark:bg-neutral-800"></div>
-        <div class="h-full w-3/4 rounded bg-neutral-200 dark:bg-neutral-800"></div>
-      </div>
+  <div v-for="i in 21" :key="i">
+    <div class="rounded-2xl overflow-hidden w-full h-[313px] skeleton"></div>
+    <div class="grid gap-1.5 pt-3 pb-4 px-1.5">
+      <div class="h-4 w-20 rounded skeleton"></div>
+      <div class="h-4 w-44 rounded skeleton"></div>
+      <div class="h-4 w-28 rounded skeleton"></div>
     </div>
   </div>
 </template>
+
+<style lang="postcss">
+.skeleton {
+  @apply bg-neutral-200 relative overflow-hidden;
+  &:before {
+    @apply absolute w-full h-full;
+    content: '';
+    background: linear-gradient(110deg, #e5e5e5 8%, #f5f5f5 18%, #e5e5e5 33%);
+    background-repeat: no-repeat;
+    background-size: 400% 400%;
+    animation: 1.5s shimmer linear infinite;
+  }
+}
+
+@keyframes shimmer {
+  0% {
+    background-position: -500px 0;
+  }
+  100% {
+    background-position: 500px 0;
+  }
+}
+</style>
