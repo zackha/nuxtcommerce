7004192b4fd674aebd566cfb6ff3f21f98da683e
Zack Hatlen <zckhtln@icloud.com>
2023-07-05 14:11:21 +0300

    fix: category page

--- pages/categories.vue
+++ pages/categories.vue
@@ -2,20 +2,21 @@
   <div class="py-8 w-full items-center flex-col">
     <div class="container m-auto">
       <div class="flex flex-row flex-wrap gap-4 justify-center">
-        <div v-for="(category, i) in categories" :key="i">
+        <NuxtLink v-for="(category, i) in categories" :key="i" class="group" :to="`/?category=${category.name}`">
           <div class="w-[444px] min-h-[306px]">
-            <div class="pb-[75%] rounded-[32px] relative overflow-hidden">
+            <div class="pb-[75%] relative overflow-hidden">
               <div class="absolute left-0 right-0 top-0 bottom-0">
                 <div class="h-full relative">
                   <NuxtImg
-                    class="object-cover absolute w-full h-full"
-                    src="https://nuxtcommerce.terodon.com/wp-content/uploads/2023/07/d08ec3ea0a65a2c40c6dcb0871ec98a3.jpg"
+                    v-if="category.image"
+                    class="object-cover absolute w-full h-full transition-all group-active:scale-95 rounded-[32px]"
+                    :src="category.image.sourceUrl"
                     loading="lazy"
-                    title="Sweatshirts" />
-                  <div class="absolute left-0 right-0 top-0 bottom-0 overflow-hidden">
+                    :title="category.name" />
+                  <div class="absolute left-0 right-0 top-0 bottom-0 overflow-hidden bg-gradient-to-t hover:from-black/40 transition-all group-active:scale-95">
                     <div class="w-full h-full bg-gradient-to-t from-black/40 items-end py-6 px-5 flex flex-row">
                       <div class="w-full justify-center flex flex-col">
-                        <div class="text-center font-semibold text-3xl">Sweatshirts</div>
+                        <div class="text-center font-semibold text-3xl">{{ category.name }}</div>
                       </div>
                     </div>
                   </div>
@@ -23,7 +24,7 @@
               </div>
             </div>
           </div>
-        </div>
+        </NuxtLink>
       </div>
     </div>
   </div>
