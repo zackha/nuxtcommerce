f1b3eb7d516ce15e8945938f305e885b29293b3e
Zack Hatlen <zckhtln@icloud.com>
2023-07-06 15:24:32 +0300

    fix

--- components/ButtonWishlist.vue
+++ components/ButtonWishlist.vue
@@ -1,15 +1,30 @@
 <template>
-  <button @click="toggleWishlist(product)">
+  <button @click="toggleWishlist">
     <div class="w-12 h-12 rounded-md ml-4 flex justify-center items-center border-2 text-[#8c8c8c] border-[#e6e6e6] dark:border-[#262626]">
-      <Icon :name="inWishlist[product.databaseId] ? 'ph:heart-fill' : 'ph:heart-bold'" size="24" />
+      <Icon :name="isWishlisted ? 'ph:heart-fill' : 'ph:heart-bold'" size="24" />
     </div>
   </button>
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
   product: Object,
 });
 
-const { toggleWishlist, inWishlist } = useWishlist();
+const theList = ref(JSON.parse(localStorage.getItem('wishlist')) || []);
+
+function updateLocalStorage() {
+  localStorage.setItem('wishlist', JSON.stringify(theList.value));
+}
+
+const isWishlisted = computed(() => theList.value.some((item) => item.databaseId === props.product.databaseId));
+
+const toggleWishlist = () => {
+  if (isWishlisted.value) {
+    theList.value = theList.value.filter((item) => item.databaseId !== props.product.databaseId);
+  } else {
+    theList.value.push(props.product);
+  }
+  updateLocalStorage();
+};
 </script>
--- pages/favorites.vue
+++ pages/favorites.vue
@@ -1,24 +1,22 @@
 <template>
   <div>
-    <div v-for="product in products" :key="product.databaseId" class="flex gap-2 items-center mb-2">
-      <div>{{ product.name }}</div>
-      <button @click="toggleWishlist(product.databaseId)">
-        <Icon :name="inWishlist[product.databaseId] ? 'iconamoon:heart-fill' : 'iconamoon:heart'" size="16" />
-      </button>
-    </div>
-
-    <br />
-
-    <div class="text-2xl">Wishlist</div>
-    <div v-for="id in wishlist" :key="id" class="flex gap-2 items-center mb-2">
-      <div>{{ wishlistProducts[id].name }}</div>
-      <button @click="toggleWishlist(id)">
-        <Icon name="iconamoon:close" size="16" />
-      </button>
+    <div v-for="product in theList" :key="product.databaseId">
+      {{ product.name }}
+      <button class="ml-2" title="Remove Item" @click="removeFromList(product.databaseId)">remove</button>
     </div>
   </div>
 </template>
 
 <script setup>
-const { products, toggleWishlist, wishlistProducts, inWishlist, wishlist } = useWishlist();
+const theList = ref([]);
+
+const removeFromList = (databaseId) => {
+  theList.value = theList.value.filter((item) => item.databaseId !== databaseId);
+  localStorage.setItem('wishlist', JSON.stringify(theList.value));
+};
+
+onMounted(() => {
+  const wishlist = localStorage.getItem('wishlist');
+  if (wishlist) theList.value = JSON.parse(wishlist);
+});
 </script>
