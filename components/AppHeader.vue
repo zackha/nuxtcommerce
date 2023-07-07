9355cd27a4c7659bad6eda1d5d41077ea7026650
Zack Hatlen <zckhtln@icloud.com>
2023-07-07 19:02:47 +0300

    Update AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -80,7 +80,7 @@
           </div>
         </div>
       </div>
-      <div @click="cart = !cart" class="hover:bg-[#e9e9e9] hover:dark:bg-[#262626] w-12 h-12 flex items-center justify-center rounded-full relative">
+      <div @click="cart = !cart" class="hover:bg-[#e9e9e9] hover:dark:bg-[#262626] w-12 h-12 flex items-center justify-center rounded-full relative cursor-pointer">
         <Icon class="text-[#5f5f5f] dark:text-[#b7b7b7]" name="fa6-solid:bag-shopping" size="22" />
         <div v-if="cart" class="w-[18px] rounded-full absolute bg-[#e60023] top-1 right-1 text-xs font-semibold leading-[16px] pb-0.5 text-center text-white">1</div>
       </div>
