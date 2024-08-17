d8a4dee2a2ab2544c8485c46c0d199edfbea0ef9
Sefa Bulak <zckhtln@icloud.com>
2024-08-17 03:50:32 +0300

    Update ButtonSelectCategory.vue

--- components/ButtonSelectCategory.vue
+++ components/ButtonSelectCategory.vue
@@ -10,7 +10,7 @@ const categories = computed(() => categoriesData.value);
 </script>
 
 <template>
-  <div class="slider-container ml-4 gap-4" v-if="categories.length">
+  <div class="slider-container ml-4 gap-4" v-if="!categories.length">
     <div class="h-[50px] min-w-36 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton"></div>
     <div class="h-[50px] relative min-w-36 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton items-center flex">
       <div class="w-10 h-10 absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
