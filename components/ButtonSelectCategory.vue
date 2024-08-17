3ba3234512fa0c75d96d1b2197ae32bbf1048025
Sefa Bulak <zckhtln@icloud.com>
2024-08-17 03:54:09 +0300

    Update ButtonSelectCategory.vue

--- components/ButtonSelectCategory.vue
+++ components/ButtonSelectCategory.vue
@@ -13,46 +13,46 @@ const categories = computed(() => categoriesData.value);
   <div class="slider-container ml-4 gap-4" v-if="!categories.length">
     <div class="h-[50px] min-w-36 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton"></div>
     <div class="h-[50px] relative min-w-36 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton items-center flex">
-      <div class="w-10 h-10 absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
+      <div class="w-[38px] h-[38px] absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
     </div>
     <div class="h-[50px] relative min-w-32 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton items-center flex">
-      <div class="w-10 h-10 absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
+      <div class="w-[38px] h-[38px] absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
     </div>
     <div class="h-[50px] relative min-w-48 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton items-center flex">
-      <div class="w-10 h-10 absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
+      <div class="w-[38px] h-[38px] absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
     </div>
     <div class="h-[50px] relative min-w-32 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton items-center flex">
-      <div class="w-10 h-10 absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
+      <div class="w-[38px] h-[38px] absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
     </div>
     <div class="h-[50px] relative min-w-60 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton items-center flex">
-      <div class="w-10 h-10 absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
+      <div class="w-[38px] h-[38px] absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
     </div>
     <div class="h-[50px] relative min-w-32 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton items-center flex">
-      <div class="w-10 h-10 absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
+      <div class="w-[38px] h-[38px] absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
     </div>
     <div class="h-[50px] relative min-w-36 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton items-center flex">
-      <div class="w-10 h-10 absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
+      <div class="w-[38px] h-[38px] absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
     </div>
     <div class="h-[50px] relative min-w-32 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton items-center flex">
-      <div class="w-10 h-10 absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
+      <div class="w-[38px] h-[38px] absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
     </div>
     <div class="h-[50px] relative min-w-32 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton items-center flex">
-      <div class="w-10 h-10 absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
+      <div class="w-[38px] h-[38px] absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
     </div>
     <div class="h-[50px] relative min-w-28 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton items-center flex">
-      <div class="w-10 h-10 absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
+      <div class="w-[38px] h-[38px] absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
     </div>
     <div class="h-[50px] relative min-w-32 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton items-center flex">
-      <div class="w-10 h-10 absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
+      <div class="w-[38px] h-[38px] absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
     </div>
     <div class="h-[50px] relative min-w-40 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton items-center flex">
-      <div class="w-10 h-10 absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
+      <div class="w-[38px] h-[38px] absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
     </div>
     <div class="h-[50px] relative min-w-32 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton items-center flex">
-      <div class="w-10 h-10 absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
+      <div class="w-[38px] h-[38px] absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
     </div>
     <div class="h-[50px] relative min-w-44 bg-neutral-200 dark:bg-neutral-800 rounded-full skeleton items-center flex">
-      <div class="w-10 h-10 absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
+      <div class="w-[38px] h-[38px] absolute rounded-full bg-neutral-300/50 dark:bg-neutral-900/40 left-2"></div>
     </div>
   </div>
   <CarouselCategories v-else :categories="categories" />
