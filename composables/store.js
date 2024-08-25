c0e337640321ccc86b7657347d46b85b97952d8d
Sefa Bulak <zckhtln@icloud.com>
2024-08-25 21:59:29 +0300

    Revert "feat: added cart functions"
    This reverts commit 7fd838fd642e8191a47e0c8e81d7a404d22fe31f.

--- composables/store.js
+++ composables/store.js
@@ -1,3 +1,5 @@
+import { LRUCache } from 'lru-cache';
+import { hash as ohash } from 'ohash';
 import { getProductsQuery } from '~/gql/queries/getProducts';
 import { getCategoriesQuery } from '~/gql/queries/getCategories';
 import { getSearchProductsQuery } from '~/gql/queries/getSearchProducts';
@@ -6,7 +8,12 @@ import { addToCartMutation } from '~/gql/mutations/addToCart';
 import { updateItemQuantitiesMutation } from '~/gql/mutations/updateItemQuantities';
 import { checkoutMutation } from '~/gql/mutations/checkout';
 
-async function fetchGraphQL(query, variables = {}) {
+const promiseCache = new LRUCache({
+  max: 500,
+  ttl: 2000 * 60 * 60,
+});
+
+async function _fetchGraphQL(query, variables = {}) {
   const { $graphql } = useNuxtApp();
   const session = localStorage.getItem('woocommerce-session');
 
@@ -17,6 +24,45 @@ async function fetchGraphQL(query, variables = {}) {
   }
 
   return await $graphql.default.request(query, variables, { 'woocommerce-session': session });
+}
+
+export function fetchGraphQL(query, variables) {
+  const hash = ohash([query, variables]);
+  const state = useState(hash, () => null);
+  const cacheEntry = promiseCache.get(hash);
+
+  if (cacheEntry) {
+    const isStale = Date.now() - cacheEntry.timestamp >= 2000 * 60 * 60;
+
+    if (!isStale) {
+      _fetchGraphQL(query, variables)
+        .then(res => {
+          if (JSON.stringify(res) !== JSON.stringify(cacheEntry.data)) {
+            state.value = res;
+            promiseCache.set(hash, { data: Promise.resolve(res), timestamp: Date.now() });
+          }
+        })
+        .catch(e => {
+          console.error('Error while revalidating:', e);
+        });
+
+      return cacheEntry.data;
+    }
+  }
+
+  const fetchPromise = _fetchGraphQL(query, variables)
+    .then(res => {
+      state.value = res;
+      promiseCache.set(hash, { data: Promise.resolve(res), timestamp: Date.now() });
+      return res;
+    })
+    .catch(e => {
+      promiseCache.delete(hash);
+      throw e;
+    });
+
+  promiseCache.set(hash, { data: fetchPromise, timestamp: Date.now() });
+  return fetchPromise;
 }
 
 //Query functions
