6cd5babd18e0f0e690f8f6266e9d4328896be925
Sefa Bulak <zckhtln@icloud.com>
2025-09-07 13:07:02 +0200

    fix: validate product api params

--- server/api/product.get.ts
+++ server/api/product.get.ts
@@ -1,9 +1,12 @@
-import { getQuery } from 'h3';
+import { createError, getQuery } from 'h3';
 import { getProductQuery } from '~/gql/queries/getProduct';
 import { requestQuery } from '~~/server/utils/wpgraphql';
 
 export default defineCachedEventHandler(async event => {
   const { slug, sku } = getQuery(event);
+  if (!slug || !sku) {
+    throw createError({ statusCode: 400, statusMessage: 'slug and sku are required' });
+  }
   return await requestQuery(getProductQuery, { slug, sku });
 }, {
   maxAge: 60 * 5,
